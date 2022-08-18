16f5cb00bf04a0e8d4f5845ab06394b552a4a46d
calipho <106757959+calipho@users.noreply.github.com>
2022-08-18 13:19:43 +0300

    Done

--- foodies/foodies/urls.py
+++ foodies/foodies/urls.py
@@ -28,10 +28,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path
 from recipe import views as foodies_views
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', foodies_views.home, name='home'),
@@ -77,3 +80,9 @@ urlpatterns = [
 
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
